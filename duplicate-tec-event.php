075cb5bb61c47ab1fe90f4c7b1affd517e5d6dce
blobaugh <blobaugh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-19 23:29:53 +0000

    Added nonce security and performed updates requested by Modern Tribe
    git-svn-id: https://plugins.svn.wordpress.org/duplicate-tec-event/trunk@574914 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- duplicate-tec-event.php
+++ duplicate-tec-event.php
@@ -1,9 +1,9 @@
 <?php
 /*
  Plugin Name: Duplicate TEC Event
- Plugin URI: 
+ Plugin URI: https://github.com/FreshMuseDesign/duplicate-tec-event
  Description: Adds the ability to duplicate an event created by Modern Tribe's The Event Calendar plugin. This plugin utilizes the TEC functions to ensure that the new event gets passed through all proper filters
- Version: 1.1
+ Version: 1.3
  Author: Ben Lobaugh
  Author URI: http://ben.lobaugh.net
  Text Domain: duplicate-tec-event
@@ -13,15 +13,24 @@ define( 'DTE_TEXT_DOMAIN', 'duplicate-tec-event' );
 
 define( 'DTE_PLUGIN_DIR', dirname( plugin_basename( __FILE__ ) ) );
 
-define( 'DTE_TRIBE_API' , WP_PLUGIN_DIR . "/the-events-calendar/lib/tribe-event-api.class.php" );
-
 load_plugin_textdomain( DTE_TEXT_DOMAIN, false, trailingslashit( DTE_PLUGIN_DIR ) . 'lang' );
 
-$eid = 0;
+
 // Ensure we are in wp-admin before performing any additional actions
 if( is_admin() ) {
+    add_action( 'admin_init', 'dte_init' );
+}
+
+/**
+ * Gets the plugin rolling
+ * 
+ * - Ensures TEC is available
+ * - Adds the 'Duplicate' action to the TEC list page
+ * - Checks to see if duplication should be performed
+ */
+function dte_init() {
     // Ensure the TEC plugin exists
-    if( file_exists( DTE_TRIBE_API ) ) {
+    if( class_exists( 'TribeEventsAPI' ) ) {
         // Setup links on events listing to duplicate
         add_filter( 'post_row_actions', 'dte_row_actions', 10, 2);
 
@@ -30,55 +39,86 @@ if( is_admin() ) {
                 && isset( $_GET['post'] )
                 && is_numeric( $_GET['post'] )
         ) {
-            //dte_duplicate_tribe_event( $_GET['post'] );
-            $eid = $_GET['post'];
-            add_action( 'admin_init', 'dte_duplicate_tribe_event' );
+            dte_duplicate_tribe_event();
+            //add_action( 'admin_init', 'dte_duplicate_tribe_event' );
         }
     } else {
         // Show alert telling user to install TEC
-        add_thickbox();
+        //add_thickbox();
+        add_action( 'admin_enqueue_scripts', 'add_thickbox' );
         add_action( 'admin_notices', 'dte_admin_notice_install_tec' );
     }
 }
 
-function dte_admin_notice_install_tec() {
-    $url = 'plugin-install.php?tab=search&s=the+events+calendar&plugin-search-input=Search+Plugins';
+/**
+ * Displays a notification in wp-admin informing the user to install TEC.
+ * Provides a link to the installer 
+ */
+function dte_admin_notice_install_tec() {    
     $url = admin_url('plugin-install.php?tab=plugin-information&plugin=the-events-calendar&TB_iframe=true&width=640&height=517');
+    
     echo '<div class="error">
        <p>You must install <a href="'.$url.'" class="thickbox onclick">The Events Calendar</a> plugin before enabling the Duplicate TEC Event plugin!</p>
     </div>';
 }
 
 
+/**
+ * Adds additional actions to the TEC listing page
+ * @param array $actions
+ * @param type $post
+ * @return string 
+ */
 function dte_row_actions( $actions, $post ) {
     // Before altering the available actions, ensure we are on the tribe events page
     if( $post->post_type != 'tribe_events' ) return $actions;
 
-    $actions['duplicate_tribe_event'] = '<a href=\''.admin_url('?post_type=tribe_events&action=duplicate_tribe_event&post='.$post->ID).'\'>Duplicate</a>';;
+    $nonce = wp_create_nonce( 'dte_duplicate_event' );
+    $actions['duplicate_tribe_event'] = '<a href=\''.admin_url('?post_type=tribe_events&action=duplicate_tribe_event&post='.$post->ID).'&_nonce=' . $nonce . '\'>Duplicate</a>';;
     
     return $actions;
 }
 
-function dte_duplicate_tribe_event( /*$event_id*/ ) {
+/**
+ * Performs the work of duplicating the TEC event
+ * @return boolean 
+ */
+function dte_duplicate_tribe_event() {
+    if( !isset( $_GET['_nonce'] ) || !wp_verify_nonce( $_GET['_nonce'], 'dte_duplicate_event' ) )
+            return false;
+    
     $event_id = $_GET['post'];
     
     if ( !class_exists( 'TribeEventsAPI' ) ) 
-        if( !file_exists( DTE_TRIBE_API ))
-                return false;
-        require_once( DTE_TRIBE_API );
+        return false;
     
     
     $event = (array)get_post( $event_id );
     unset( $event['ID'] ); // Remove ID to prevent an update from happening
     $event['post_status'] = 'draft';
-    
+        
     $meta = get_post_custom( $event_id );
-   
+    
     // Flatten out the meta array (WTF?)
     $fmeta = array();
     foreach( $meta AS $k => $v ) {
         $fmeta[$k] = $v[0];
     }
+    
+    
+    // TEC expects a couple fields to exist without the _ upon creation
+    $event['EventStartDate'] = date( 'Y-m-d', strtotime( $fmeta['_EventStartDate'] ) );
+   // unset( $event['_EventStartDate'] ); // Do not accidentally overwrite the db!
+    $event['EventStartHour'] = date( 'h', strtotime( $fmeta['_EventStartDate'] ) );
+   // unset( $event['_EventStarHour'] ); // Do not accidentally overwrite the db!
+    $event['EventStartMinute'] = date( 'i', strtotime( $fmeta['_EventStartDate'] ) );
+   // unset( $event['_EventStartMinute'] ); // Do not accidentally overwrite the db!
+    $event['EventEndDate'] = date( 'Y-m-d', strtotime( $fmeta['_EventEndDate'] ) );
+   // unset( $event['_EventEndDate'] ); // Do not accidentally overwrite the db!
+    $event['EventEndHour'] = date( 'h', strtotime( $fmeta['_EventEndDate'] ) );
+   // unset( $event['_EventEndDate'] ); // Do not accidentally overwrite the db!
+    $event['EventEndMinute'] = date( 'j', strtotime( $fmeta['_EventEndDate'] ) );
+   
     
     $event = array_merge( $event, $fmeta );
     
@@ -103,10 +143,5 @@ function dte_duplicate_tribe_event( /*$event_id*/ ) {
     }
     
     // Send back to the original page
-    header("Location: " . admin_url("edit.php?post_type=tribe_events" ) );
-}
-
-
-function d($w) {
-    echo '<pre>'; var_dump($w); echo '</pre>';
+    wp_redirect(admin_url("edit.php?post_type=tribe_events" ) ); exit;
 }
